2b12b9e938f85c6779d0f53b2c591eb805b83f5b
drcImpacta <107211036+drcImpacta@users.noreply.github.com>
2022-09-25 10:12:10 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-vini"
+    bucket = "terraform-state-doug"
     key    = "terraform-network-catapimba.tfstate"
     region = "us-east-1"
   }
